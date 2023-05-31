86ee464464fe288f06366f4d72dcad67eb27e361
basitvolley <7527kashifkhan@gmail.com>
2023-05-31 15:44:30 +0530

    MSAgroups

---
+++ home.js
@@ -0,0 +1,9 @@
+const header= document.querySelector("#header");
+
+const sticky =header.offsetTop;
+
+console.log(sticky);
+
+window.addEventListener('scroll'), () =>;{
+    console.log(windows.pageYoffset);
+});
